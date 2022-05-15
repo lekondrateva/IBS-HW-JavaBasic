ee9a01799c9ac3bdfe6afffa6cc99bbe0be18c24
lekondrateva <umpalus@gmail.com>
2022-05-15 14:35:25 +0500

    Task #5

---
+++ src/com/lena/idea/Calculator.java
@@ -0,0 +1,35 @@
+package com.lena.idea;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void calculate() throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число x");
+        double x = in.nextDouble();
+        System.out.println("Введите число y");
+        double y = in.nextDouble();
+        System.out.println("Введите операцию");
+        char operand = (char) System.in.read();
+        switch (operand) {
+            case '+':
+                System.out.println("Результат " + (x + y));
+                break;
+            case '-':
+                System.out.println("Результат " + (x - y));
+                break;
+            case '*':
+                System.out.println("Результат " + (x * y));
+                break;
+            case '/':
+                if (y != 0) {
+                    System.out.println("Результат " + (x / y));
+                    break;
+                } else {
+                    System.out.println("Нельзя разделить на 0");
+                    break;
+                }
+        }
+    }
+}
--- src/com/lena/idea/Main.java
+++ src/com/lena/idea/Main.java
@@ -1,9 +1,9 @@
 package com.lena.idea;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         //Задание №3 - работа с массивом
         int[] array1 = {1, 2, 3, 4, 5};
@@ -12,5 +12,7 @@ public class Main {
         myArrays.sumFirstAndMid(array1);
         //Задание №4 - операторы
         Operators.printAvg();
+        //Задание №5 - калькулятор
+        Calculator.calculate();
     }
 }
