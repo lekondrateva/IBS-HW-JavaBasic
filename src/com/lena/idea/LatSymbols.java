31338aa84b3c1f1069e06f3b8567b0cccca17077
lekondrateva <umpalus@gmail.com>
2022-06-04 18:43:49 +0500

    Задание №13 (8 раздел)

--- src/com/lena/idea/LatSymbols.java
+++ src/com/lena/idea/LatSymbols.java
@@ -1,18 +1,20 @@
 package com.lena.idea;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class LatSymbols {
     public static void printOnlyLatWords() {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку");
         String str = in.nextLine();
-        String[] strMass= str.split(" ");
+        String[] strMass = str.split(" ");
+        int count = 0;
         for (String mass : strMass) {
             if (mass.matches("^[a-zA-Z]*$")) {
                 System.out.println(mass);
+                count += 1;
             }
         }
+        System.out.println(count);
     }
 }
